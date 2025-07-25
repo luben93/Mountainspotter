dc553aa1b48dfa5530c8b1e3eb32b87c5bbf4a84
lucas persson <lucas.persson@uc.se>
2025-07-25 05:29:47 +0200

    restart kmp

---
+++ composeApp/src/commonTest/kotlin/org/luben93/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.luben93
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
